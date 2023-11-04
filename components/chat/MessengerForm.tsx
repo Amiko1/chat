4ba92967a5325daf10f499d3ccd57265fb59cb1e
Amirani <amiraniburjanadze@gmail.com>
2023-11-04 04:19:22 +0400

    make MessengerForm generic and reusable

--- components/chat/MessengerForm.tsx
+++ components/chat/MessengerForm.tsx
@@ -3,11 +3,25 @@
 import { Input } from "@/components/ui/input";
 import DarkButton from "../ui/DarkButton";
 
-export default function MessengerForm() {
+type Props = {
+  text: string;
+  onClick: Function;
+  onChange: Function;
+};
+
+export default function MessengerForm({ text, onClick, onChange }: Props) {
+  const handler = () => {
+    onChange(text);
+  };
   return (
     <>
-      <Input className="" placeholder="Type your message here." />;
-      <DarkButton>SEND</DarkButton>
+      <Input
+        className=""
+        placeholder="Type your message here."
+        onChange={handler}
+        value={text}
+      />
+      ;<DarkButton onClick={onClick}>SEND</DarkButton>
     </>
   );
 }
